4723c5d0794d3a83e9522b60d8b5f796ba537fdb
jdlarsen-UA <jdlarsen@email.arizona.edu>
2016-08-03 15:49:06 -0700

    Added support for multiple colloids, bug fixed colloid updating

--- grid_math_dev/LB_Colloid.py
+++ grid_math_dev/LB_Colloid.py
@@ -31,12 +31,24 @@ class Colloid:
         self.xposition = list(np.random.rand(1)*xlen*resolution)
         self.yposition = [0]
         self.resolution = resolution
+        self.storey = [self.yposition[0]]
+        self.storex = [self.xposition[0]]
+        self.time = [0]
 
     def _append_xposition(self, item):
-        self.xpositon = self.xposition.append(item)
+        self.xposition.append(item)
 
     def _append_yposition(self, item):
-        self.xpositon = self.yposition.append(item)
+        self.yposition.append(item)
+
+    def _append_storex(self, item):
+        self.storex.append(item)
+
+    def _append_storey(self, item):
+        self.storey.append(item)
+        
+    def _append_time(self, item):
+        self.time.append(item)
 
     def update_position(self, xvelocity, yvelocity, ts):
         '''
@@ -46,13 +58,22 @@ class Colloid:
         irx = self.xposition[-1]
         iry = self.yposition[-1]
         idxrx = int(self.xposition[-1]//self.resolution) 
-        idxry = int(self.yposition[-1]//-self.resolution) #  negative accounts for grid indexes.
+        idxry = int(self.yposition[-1]//-self.resolution)#  negative accounts for grid indexes.
         xv = xvelocity[idxry][idxrx]
         yv = yvelocity[idxry][idxrx]
         deltarx = xv/ts
         deltary = yv/ts
         rx = irx + deltarx
-        ry = iry + deltary 
+        ry = iry + deltary
+
+        # Use a check to make sure colloid does not leave domain on first iteration
+        # move colloid to a new location to if it leaves domain
+        if ry >= 0:
+            rx = np.random.rand(1)[0]*xlen*self.resolution
+            ry = 0.
+        else:
+            pass
+        
         self._append_xposition(rx)
         self._append_yposition(ry)
 
@@ -60,84 +81,129 @@ class Colloid:
         self.xposition = [self.xposition[-1]]
         self.yposition = [self.yposition[-1]]
 
+    def store_position(self, timer):
+        self._append_time(timer)
+        self._append_storex(self.xposition[-1])
+        self._append_storey(self.yposition[-1])
+        
+    def print_positions(self):
+        print self.xposition[-1], self.yposition[-1]
 
-LB = cs.HDF5_reader('Synthetic255.hdf5')
 
-#### gridsplit needs to be one of the many config options!
-gridsplit = 10
-gridres = 1e-6/gridsplit
+class Output:
+    def __init__(self, fname):
+        self.fname = fname
 
-LBy = cs.LBVArray(LB.yu, LB.imarray)
-LBx = cs.LBVArray(LB.xu, LB.imarray)
+    
 
-#### interpolate over grid array and interpolate veloctity profiles ####
-#Col_vp = interp_v(LBvp, gridsplit)
-LBy = cs.InterpV(LBy, gridsplit)
-LBx = cs.InterpV(LBx, gridsplit)
-Col_img = cs.InterpV(LB.imarray, gridsplit)
+if __name__ == '__main__':        
+    LB = cs.HDF5_reader('Synthetic255.hdf5')
 
-### Use grids function to measure distance from pore space and correct
-### boundaries for interpolation effects. 
-Grids = cs.Gridarray(Col_img, gridres, gridsplit)
-xArr = Grids.gridx
-yArr = Grids.gridy
+    #### gridsplit needs to be one of the many config options!
+    gridsplit = 10
+    gridres = 1e-6/gridsplit
 
-xvArr = Grids.vector_x
-yvArr = Grids.vector_y
+    LBy = cs.LBVArray(LB.yu, LB.imarray)
+    LBx = cs.LBVArray(LB.xu, LB.imarray)
 
-xArr[LBx == 0.] = np.nan
-yArr[LBy == 0.] = np.nan
-xvArr[LBx == 0.] = np.nan
-yvArr[LBy == 0.] = np.nan
+    #### interpolate over grid array and interpolate veloctity profiles ####
+    #Col_vp = interp_v(LBvp, gridsplit)
+    LBy = cs.InterpV(LBy, gridsplit)
+    LBx = cs.InterpV(LBx, gridsplit)
+    Col_img = cs.InterpV(LB.imarray, gridsplit)
 
-# Begin calling colloid mathematics #
+    ### Use grids function to measure distance from pore space and correct
+    ### boundaries for interpolation effects. 
+    Grids = cs.Gridarray(Col_img, gridres, gridsplit)
+    xArr = Grids.gridx
+    yArr = Grids.gridy
 
-cfactor = cm.Gap(xArr, yArr)
+    xvArr = Grids.vector_x
+    yvArr = Grids.vector_y
 
-velocity = cm.Velocity(LBx, LBy, 1., gridres)
-LBx = velocity.xvelocity
-LBy = velocity.yvelocity
+    xArr[LBx == 0.] = np.nan
+    yArr[LBy == 0.] = np.nan
+    xvArr[LBx == 0.] = np.nan
+    yvArr[LBy == 0.] = np.nan
 
-drag_forces = cm.Drag(LBx, LBy, LBx, LBy, cfactor.f1, cfactor.f2,
-                      cfactor.f3, cfactor.f4, xvArr, yvArr) 
+    # Begin calling colloid mathematics #
 
-brownian = cm.Brownian(xArr, yArr, cfactor.f1, cfactor.f4)
+    cfactor = cm.Gap(xArr, yArr)
 
-dlvo = cm.DLVO(xArr, yArr, xvArr=xvArr, yvArr=yvArr)
+    velocity = cm.Velocity(LBx, LBy, 1., gridres)
+    LBx = velocity.xvelocity
+    LBy = velocity.yvelocity
 
-gravity = cm.Gravity()
-bouyancy = cm.Bouyancy()
+    # Initial setup block to estimate colloid velocity for drag_force calc. #
+    drag_forces = cm.Drag(LBx, LBy, LBx, LBy, cfactor.f1, cfactor.f2,
+                          cfactor.f3, cfactor.f4, xvArr, yvArr)
 
-physicalx = brownian.brownian_x + drag_forces.drag_x
-physicaly = brownian.brownian_y + drag_forces.drag_y + gravity.gravity + bouyancy.bouyancy
+    brownian = cm.Brownian(xArr, yArr, cfactor.f1, cfactor.f4)
 
-dlvox = dlvo.EDLx + dlvo.LVDWx + dlvo.LewisABx
-dlvoy = dlvo.EDLy + dlvo.LVDWy + dlvo.LewisABy
+    dlvo = cm.DLVO(xArr, yArr, xvArr=xvArr, yvArr=yvArr)
 
-fx = dlvox + physicalx
-fy = dlvoy + physicaly
+    gravity = cm.Gravity()
+    bouyancy = cm.Bouyancy()
 
-vx = cm.ForceToVelocity(fx)
-vy = cm.ForceToVelocity(fy)
+    physicalx = brownian.brownian_x + drag_forces.drag_x
+    physicaly = brownian.brownian_y + drag_forces.drag_y + gravity.gravity + bouyancy.bouyancy
 
-vx = vx.velocity + LBx
-vy = vy.velocity + LBy
-xlen = len(Col_img)
-x = Colloid(xlen, gridres)
+    dlvox = dlvo.EDLx + dlvo.LVDWx + dlvo.LewisABx
+    dlvoy = dlvo.EDLy + dlvo.LVDWy + dlvo.LewisABy
 
-timer = 0
-while timer < 20000:
-    x.update_position(vx, vy, 1)
-    x.update_position(vx, vy, 1)
-    timer += 1
-    if timer%10000 == 0.:
-        print timer
-print x.yposition[:2], x.yposition[-3:]
-plt.imshow(vx, interpolation='nearest', vmin=-1e-13, vmax=1e-13)
-plt.plot(np.array(x.xposition)/gridres, np.array(x.yposition)/-gridres, 'ko', ms=2)
-plt.colorbar()
-plt.show()
+    fx = dlvox + physicalx
+    fy = dlvoy + physicaly
 
-plt.imshow(vy, interpolation='nearest', vmin=-1e-13, vmax=1e-13)
-plt.colorbar()
-plt.show()
+    vx = cm.ForceToVelocity(fx)
+    vy = cm.ForceToVelocity(fy)
+
+    vx = vx.velocity + LBx
+    vy = vy.velocity + LBy
+    # End setup block ##########################################################
+
+    # Start velocity block by recalculating drag_forces to get new colloid velocity profile!
+
+    drag_forces = cm.Drag(LBx, LBy, vx, vy, cfactor.f1, cfactor.f2,
+                          cfactor.f3, cfactor.f4, xvArr, yvArr)
+
+    physicalx = brownian.brownian_x + drag_forces.drag_x
+    physicaly = brownian.brownian_y + drag_forces.drag_y + gravity.gravity + bouyancy.bouyancy
+
+    dlvox = dlvo.EDLx + dlvo.LVDWx + dlvo.LewisABx
+    dlvoy = dlvo.EDLy + dlvo.LVDWy + dlvo.LewisABy
+
+    fx = dlvox + physicalx
+    fy = dlvoy + physicaly
+
+    vx = cm.ForceToVelocity(fx)
+    vy = cm.ForceToVelocity(fy)
+
+    vx = vx.velocity + LBx
+    vy = vy.velocity + LBy
+    # End velocity profile block ##############################################
+
+    xlen = len(Col_img)
+    x = [Colloid(xlen, gridres) for i in range(1)]
+
+    timer = 0
+    while timer < 500000:
+        for col in x:
+            col.update_position(vx, vy, 0.0001)
+        timer += 1
+        if timer%100 == 0.:
+            print timer
+            for col in x:
+                col.store_position(timer)
+                col.strip_positions()
+        
+    #print x.yposition[:2], x.yposition[-3:]
+    #plt.imshow(vx, interpolation='nearest', vmin=-1e-13, vmax=1e-13)
+    #plt.colorbar()
+    #plt.show()
+
+    plt.imshow(vy, interpolation='nearest', vmin=-1e-13, vmax=1e-13)
+    for col in x:
+        plt.plot(np.array(col.storex)/gridres, np.array(col.storey)/-gridres, 'kD',
+                 ms=8)
+    plt.colorbar()
+    plt.show()
