b9c0eddfb62bda1931f81dca083de75fb5e45f81
JLarsen <jdlarsen@email.arizona.edu>
2017-06-11 19:58:35 -0700

    Added Colloid_output.py to SVN for future development

---
+++ Colloids/Colloid_output.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# todo: return an axis object and dump the data into csv for further processing?
+
+class Breakthrough(object):
+    pass
+
+
+class ADE(object):
+    pass
+
+
+class DistributionFunction(object):
+    pass
+
+
+class DLVOPlot(object):
+    pass
+
+
+class CCDLVOPlot(object):
+    pass
+
+
+class CCDLVOMesh(object):
+    pass
+
+
+class ColloidVelocity(object):
+    pass
+
+
