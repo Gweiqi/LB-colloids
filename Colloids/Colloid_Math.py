Fix colloid-colloid hamaker constant calculation

Colloids/Colloid_Math.py
@@ -784,7 +784,8 @@ class ColloidColloid(object):
         Returns:
             dvlo: (np.ndarray) dlvo interaction force from colloids
         """
-        Na = 6.022e23
+        kb = 1.31e-23
+
         if arr_type.lower() == "x":
             c_arr = self.x_distance_array
         elif arr_type.lower() == "y":
@@ -792,12 +793,11 @@ class ColloidColloid(object):
         else:
             raise TypeError("arr_type {} is not valid".format(arr_type))
 
-        # todo: check Hamaker constant calculation! Should be approx 1e-20
-        A = 384. * np.pi * c_arr * self.debye * self.__params['T']\
-            * self.ionic_strength * self.colloid_potential * self.colloid_potential\
-            * np.exp(-self.debye * np.abs(c_arr))
 
-        A = np.ones(c_arr.shape) * 1e-20
+        A = 384. * np.pi * c_arr * kb * self.__params['T']\
+            * self.ionic_strength * self.colloid_potential * self.colloid_potential \
+            * np.exp(-self.debye * np.abs(c_arr))/ (self.debye * self.debye)
+
 
         lwdv0 = -A / 6.
         lvdw1 = (2. * self.__params['ac'] ** 2.) / (self.__params['ac'] ** 2. + 4. * self.__params['ac'] * c_arr)

Colloids/Colloid_output.py
@@ -297,9 +297,8 @@ class ADE(object):
         from scipy.optimize import leastsq, minimize, least_squares
         a = self.ncol
         l = self.ylen * self.resolution
-        t = self.pdf['nts']
         v = self.uy
-        pdf = self.pdf['ncol'] / self.ncol
+        pdf, t = self.__prep_data()
         x0 = np.array([0.01, 0.01])
 
         return least_squares(self.__jury_residuals, x0,
@@ -346,7 +345,44 @@ class ADE(object):
         x[0] = 0
         return x #(eq0 / eq1) * np.exp(eq2 / eq3)
 
+    def __prep_data(self):
+        """
+        Prepares breakthrough data by stripping off trailing
+        zeros.
 
+        Returns:
+            pdf = (np.array) stripped pdf
+            t = (np.array) times
+        """
+        strip_idx = None
+        seq = False
+        bt = False
+        for idx, rec in enumerate(self.pdf):
+            if not bt:
+                if rec['ncol'] != 0:
+                    bt = True
+                else:
+                    pass
+            else:
+                if rec['ncol'] == 0:
+                    if not seq:
+                        strip_idx = idx
+                        seq = True
+                    else:
+                        pass
+                else:
+                    seq = False
+                    strip_idx = None
+
+        if strip_idx is not None:
+            pdf = self.pdf['ncol'][:strip_idx + 1] / self.ncol
+            time = self.pdf['nts'][:strip_idx + 1]
+
+        else:
+            pdf = self.pdf['ncol'] / self.ncol
+            time = self.pdf['nts']
+
+        return pdf, time
 
 
 class ModelPlot(object):