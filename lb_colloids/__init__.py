befecfd47b57bb6586855c9b1c47cdb8465ce05f
JLarsen <jdlarsen@email.arizona.edu>
2017-07-06 13:04:03 -0700

    Packaging setup version 1 complete. Not tested yet

--- lb_colloids/__init__.py
+++ lb_colloids/__init__.py
@@ -1,5 +1,7 @@
-__name__ = "lb_colloids"
-__author__ = "Joshua D Larsen"
-__version__ = "0.1"
+from .Colloids import LB_Colloid as colloids
+from .Colloids import Colloid_IO as io
+from .Colloids import Colloid_Math as colloid_math
+from .Colloids import Colloid_output as output
 
-long_description = """ """
+from .LB import LB_2Dimage as lb_image
+from .LB.LB_2Dpermeability import LB2DModel
