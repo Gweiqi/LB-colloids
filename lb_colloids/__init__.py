7c99e7b4f15712d957d758a8e635ff1df57b2bf5
JLarsen <jdlarsen@email.arizona.edu>
2017-07-06 14:15:25 -0700

    Updates to LBCompile.sh and __init__.py

--- lb_colloids/__init__.py
+++ lb_colloids/__init__.py
@@ -2,10 +2,10 @@ import Colloids
 import LB
 from .Colloids import LB_Colloid as ColloidModel
 from .Colloids import Colloid_IO as cIO
-from .Colloids import Colloid_Math as colloid_math
+from .Colloids import Colloid_Math as ColloidMath
 from .Colloids import Colloid_Setup
-from .Colloids import Colloid_output as output
+from .Colloids import Colloid_output as Output
 
-from .LB import LB_2Dimage as lb_image
+from .LB import LB_2Dimage as LBImage
 from .LB.LB_2Dpermeability import LB2DModel
 
