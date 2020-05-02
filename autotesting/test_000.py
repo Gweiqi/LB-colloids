18afcfe8fdea97640d82fccfd229870aa365c92a
JLarsen <jdlarsen@email.arizona.edu>
2020-05-01 22:31:49 -0700

    Added autotesting skeleton to repository

---
+++ autotesting/test_000.py
@@ -0,0 +1,19 @@
+from lb_colloids import LBImage
+from lb_colloids import LB2DModel
+import matplotlib.pyplot as plt
+from lb_colloids import cIO
+from lb_colloids import ColloidModel
+import os
+import time
+
+
+def test_setup():
+	pass
+
+	
+def test_imports():
+	pass
+	
+
+if __name__ == "__main__":
+	pass
---
+++ autotesting/test_001.py
@@ -0,0 +1,27 @@
+from lb_colloids import LBImage
+from lb_colloids import LB2DModel
+import matplotlib.pyplot as plt
+from lb_colloids import cIO
+from lb_colloids import ColloidModel
+import os
+import time
+
+
+def test_psphere():
+	pass
+
+	
+def test_lb_image():
+	pass
+
+
+def test_lb_model():
+	pass
+	
+	
+def test_colloid_model():
+	pass
+	
+
+if __name__ == "__main__":
+	pass
